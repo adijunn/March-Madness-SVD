87184b8264dbeaf8516d033e21f79a0d372f3ccc
Aditya Ganapathi <adivganapathi@Adityas-MacBook-Pro-2.local>
2019-03-18 17:23:44 -0700

    Basic pca black box

---
+++ pca.py
@@ -0,0 +1,11 @@
+import sklearn
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv()
+dataset.head()
+
+sc = StandardScaler()
+pca = PCA()
+X_train = sc.fit_transform(X_train) 
+X_train = sc.fit_transform(X_train) 
